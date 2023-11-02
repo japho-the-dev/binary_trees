40e3910d4be758924534ee17cadf6e7cd91fcd28
JAPHETH <japho0740@gmail.com>
2023-11-02 01:10:39 -0700

    Add solutions

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function that calculates the size of a binary
+ * @tree: the tree to calculate the size of
+ * Return: returns 0 if the tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+
+	size += 1;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+
+	return (size);
+}
