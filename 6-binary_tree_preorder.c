931eddad42e5c5bf1a96a6295a6a1031f8db7504
JAPHETH <japho0740@gmail.com>
2023-11-02 00:53:21 -0700

    Add solutions

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree 
+ * binary_tree_preorder - function that goes through a binary tree
  * using pre-order traversal
  * @tree: tree to traverse through
  * @func: pointer to a function to call for each node
